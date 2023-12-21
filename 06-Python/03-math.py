ec7348357f0110cf3bb3fb3587f9883cab583b84
kali127001 <kalki8358@gmail.com>
2023-12-21 20:43:22 +0530

    Add basic maths

---
+++ 06-Python/03-math.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+print(50 + 50); # add
+print(50 - 50); # subtract
+print(50 * 50); # multiply
+print(50 / 50); # divide
+
+print(50 + 50 - 50 * 50 / 50); # PEDMAS => Parenthesis, Exponent, Divide, Multiply, Add, Subtract
+
+print(50 ** 2) # exponents
+
+print(50 % 6) # remainder
+
+print(50 // 6); # no remainder
