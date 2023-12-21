057dba6b72f51fe9366f98dba13e46f3227182b0
kali127001 <kalki8358@gmail.com>
2023-12-21 20:31:20 +0530

    Add Strings in python

---
+++ 06-Python/02-strings.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# print string
+print("Hello, World!");
+print('Hello, World!');
+
+print("""This is a 
+multi-line string!"""); # triple quote for multi-line
+
+print("Adding new line.\n"); # new line
+
+print("This string is " + "awesome!"); # we can also concatenate
